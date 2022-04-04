70e7e10b3eddce007b601ac2d200d07dfa9fe8ed
Khoa <khoadinh2k@gmail.com>
2022-04-03 23:42:20 -0500

    Added Computer Response and Winner Announce Functions

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ let roundCounter = 0;
 let scorePlayer = 0;
 let scoreComputer = 0;
 
+const computerResponse = document.querySelector('.computer-response');
+const winnerAnnounce = document.querySelector('.winner-announce');
+
 function resetGame() {
     roundCounter = 0;
     scorePlayer = 0;
     scoreComputer = 0;
+    computerResponse.innerHTML = '';
+    winnerAnnounce.innerHTML = '';
+
 }
 
 function gameLose(){
@@ -30,6 +36,18 @@ function gameTie(){
     console.log(`Tie!\n Round Counter : ${roundCounter}\n Score: \n Player: ${scorePlayer} | Computer: ${scoreComputer}`);
 }
 
+function computerResponseMech(a) {
+    if (a == 0) {
+        computerResponse.innerHTML = 'Computer Chooses: <img src="Images/rock.png">';
+    }
+    else if (a == 1) {
+        computerResponse.innerHTML = 'Computer Chooses: <img src="Images/paper.png">'
+    }
+    else {
+        computerResponse.innerHTML = 'Computer Chooses: <img src="Images/scissors.png">'
+    }
+}
+
 
 function playRound(a) {
     const playerSelection = a;
@@ -46,6 +64,7 @@ function playRound(a) {
         else if (computerSelection == 2){
             gameWin();
         }
+        computerResponseMech(computerSelection);
     }
     else if (playerSelection == 1){
         if (computerSelection == 0){
@@ -57,6 +76,7 @@ function playRound(a) {
         else if (computerSelection == 2){
             gameLose();
         }
+        computerResponseMech(computerSelection);
     }    
     else {
         if (computerSelection == 0){
@@ -68,5 +88,13 @@ function playRound(a) {
         else if (computerSelection == 2){
             gameTie();
         }
+        computerResponseMech(computerSelection);
+    }
+
+    if (scorePlayer == 5) {
+        winnerAnnounce.innerHTML = 'You Win!';
+    }
+    else if (scoreComputer == 5) {
+        winnerAnnounce.innerHTML = 'Computer Wins!';
     }
 }
